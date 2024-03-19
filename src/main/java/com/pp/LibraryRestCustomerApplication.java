5c30e3df5668062f248c737e5a51733e172f10f5
prakash <pr0018908@gmail.com>
2024-03-19 22:34:41 +0530

    main added

---
+++ src/main/java/com/pp/LibraryRestCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRestCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRestCustomerApplication.class, args);
+	}
+
+}
